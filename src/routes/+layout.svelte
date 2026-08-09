457e972bb294516dbcc6ef922a2989d0d5eff477
Collin DiMaio <collindimaio@Collins-Mac-mini.local>
2026-08-09 07:20:27 -0400

    adjust styles and create a navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,4 +8,29 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
+
+	
+<div class="topnav">
+	<a class="nav" href="/">Home</a>
+	<a class="nav" href="/blog">Blog</a>
+</div>
+
 {@render children()}
+
+
+
+
+<style>
+	.topnav {
+		background-color: #474747;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-evenly;
+	}
+
+	.nav {
+		color: #b8b8b8;
+		text-decoration: none;
+		justify-content: space-evenly;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit my svelte site</p>
+<script>
+
+</script>
+
+<style>
+* {
+    font-family: 'Source Code Pro', monospace;
+}
+
+body {
+    background-color: #0a0a0a;
+    color: #b8b8b8;
+    
+}
+</style>
+<body>
+    <div class="header">
+        <h1>Collin DiMaio</h1>
+        <h3>Portfolio</h3>
+        <p>This is my portfolio for projects and things</p>
+    </div>
+</body>
