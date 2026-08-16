06802b20d6dd09ada055e83e8aa9a3ba0a726330
Collin DiMaio <collindimaio@Collins-Mac-mini.local>
2026-08-16 19:00:19 -0400

    install tailwindcss and refactor code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,19 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
+	import "../app.css";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	
 </svelte:head>
 
 
-	
-<div class="topnav">
-	<a class="nav" href="/">Home</a>
-	<a class="nav" href="/blog">Blog</a>
+<div class="bg-slate-700">
+	<a class="bg-cyan-400 hover:bg-cyan-500" href="/">Home</a>
+	<a class="bg-amber-500 hover:bg-amber-600" href="/blog">Blog</a>
 </div>
 
 {@render children()}
-
-
-
-
-<style>
-	.topnav {
-		background-color: #474747;
-		display: flex;
-		flex-direction: row;
-		justify-content: space-around;
-		margin: 50px;
-		padding: 15px;
-		border-radius: 15px;
-	}
-
-	.nav {
-		color: #b8b8b8;
-		text-decoration: none;
-	}
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,27 +3,15 @@
     import ProjectCard from '$lib/components/ProjectCard.svelte';
 </script>
 
-<style>
-:global(*) {
-    font-family: 'Source Code Pro', monospace;
-}
 
-:global(body) {
-    background-color: #0a0a0a;
-    color: #b8b8b8;
-    
-}
-</style>
-<body>
-    <div class="header">
-        <h1>Collin DiMaio</h1>
-        <h3>Software Engineer</h3>
-        <p><a href="https://github.com/CollinDiMaio">Github</a> | <a href="https://gitlab.com/wgu-gitlab-environment/student-repos/cdimai6">Gitlab</a> | dimaiocollin@gmail.com</p>
-    </div>
+<div class="header">
+    <h1>Collin DiMaio</h1>
+    <h3>Software Engineer</h3>
+    <p><a href="https://github.com/CollinDiMaio">Github</a> | <a href="https://gitlab.com/wgu-gitlab-environment/student-repos/cdimai6">Gitlab</a> | dimaiocollin@gmail.com</p>
+</div>
 
-    <div class="card-box">
-        {#each projects as project}
-            <ProjectCard {project} />
-        {/each}
-    </div>
-</body>
+<div class="card-box">
+    {#each projects as project}
+        <ProjectCard {project} />
+    {/each}
+</div>
