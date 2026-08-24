035c858a12e45cf4718a943e80da06a929fcb1fb
Collin DiMaio <collindimaio@Collins-Mac-mini.local>
2026-08-24 19:58:54 -0400

    add more styling for project card

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
     import type { Project } from '$lib/data/projects';
-    import placeholder from '$lib/assets/pexels-talal-10920328.jpg';
 
     let { project }: { project: Project } = $props();
 </script>
 <!--TODO: Style project cards-->
-<div class="min-w-2xs max-w-s">
+<div class="w-1/4 h-64">
     <img src={project.image} alt={project.id} />
 
     <p class="project-title">{project.title}</p>
